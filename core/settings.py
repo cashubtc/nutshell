98d5485bddbb74a77d6f52884d6292a43a78bfaa
callebtc <93376500+callebtc@users.noreply.github.com>
2022-09-17 14:25:17 +0300

    ssl

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,10 @@ MINT_SERVER_PORT = env.int("MINT_SERVER_PORT", default=3338)
 MINT_HOST = env.str("MINT_HOST", default="127.0.0.1")
 MINT_PORT = env.int("MINT_PORT", default=3338)
 
-MINT_URL = f"http://{MINT_HOST}:{MINT_PORT}"
+if MINT_HOST in ["localhost", "127.0.0.1"]:
+    MINT_URL = f"http://{MINT_HOST}:{MINT_PORT}"
+else:
+    MINT_URL = f"https://{MINT_HOST}:{MINT_PORT}"
 
 LNBITS_ENDPOINT = env.str("LNBITS_ENDPOINT", default=None)
 LNBITS_KEY = env.str("LNBITS_KEY", default=None)
