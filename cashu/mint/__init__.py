bdd1b90aeb3b53b183220d63de8ebd9531ea4601
callebtc <93376500+callebtc@users.noreply.github.com>
2022-10-10 19:10:45 +0200

    reset derivation path

--- cashu/mint/__init__.py
+++ cashu/mint/__init__.py
@@ -1,4 +1,4 @@
 from cashu.core.settings import MINT_PRIVATE_KEY
 from cashu.mint.ledger import Ledger
 
-ledger = Ledger(MINT_PRIVATE_KEY, "data/mint", derivation_path="0")
+ledger = Ledger(MINT_PRIVATE_KEY, "data/mint")
