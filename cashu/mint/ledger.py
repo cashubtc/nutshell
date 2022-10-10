224e5c44ca39f797474759550a48a24aac549862
callebtc <93376500+callebtc@users.noreply.github.com>
2022-10-10 21:23:12 +0200

    add derivation path

--- cashu/mint/ledger.py
+++ cashu/mint/ledger.py
@@ -117,6 +117,7 @@ class Ledger:
         C = PublicKey(bytes.fromhex(proof.C), raw=True)
 
         # backwards compatibility with old hash_to_curve
+        # old clients do not send a version
         if not context.get("version"):
             return legacy.verify_pre_0_3_3(secret_key, C, proof.secret)
         return b_dhke.verify(secret_key, C, proof.secret)
