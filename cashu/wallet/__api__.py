7eb76ce8e1d4b701fc2df8b4dccbfa26807c42ab
tim <>
2022-11-26 11:14:51 +0000

    add logging to api

--- cashu/wallet/__api__.py
+++ cashu/wallet/__api__.py
@@ -1,6 +1,8 @@
-from cashu.wallet.api import app as wallet_api
+import logging
 from fastapi import FastAPI
+from cashu.wallet.api import app as wallet_api
 
 # add the api to the main app
 app = FastAPI()
 app.include_router(wallet_api, tags=["wallet"])
+logging.basicConfig(format="%(asctime)s\t%(levelname)s\t%(name)s %(lineno)d -- %(message)s", level=logging.INFO)
