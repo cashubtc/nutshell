291b73ffa1e0c65908884d31ee77c060158c6149
callebtc <93376500+callebtc@users.noreply.github.com>
2023-01-19 12:56:39 +0100

    back to normal mint running

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -12,7 +12,10 @@ from cashu.wallet import migrations
 from cashu.wallet.wallet import Wallet
 from cashu.wallet.wallet import Wallet as Wallet1
 from cashu.wallet.wallet import Wallet as Wallet2
-from tests.conftest import SERVER_ENDPOINT, mint
+
+# from tests.conftest import SERVER_ENDPOINT, mint
+
+SERVER_ENDPOINT = "http://localhost:3338"
 
 
 async def assert_err(f, msg):
