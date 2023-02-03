53aa73b7c729ded836bb2cdbe68a688b5b0e0beb
calle <93376500+callebtc@users.noreply.github.com>
2023-02-03 23:44:05 +0100

    Fix/multimind_redeem_unused_keysets (#108)
    * multmind fix
    
    * bump version
    
    * fix verify_mints

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ entry_points = {"console_scripts": ["cashu = cashu.wallet.cli:cli"]}
 
 setuptools.setup(
     name="cashu",
-    version="0.9.0",
+    version="0.9.1",
     description="Ecash wallet and mint for Bitcoin Lightning",
     long_description=long_description,
     long_description_content_type="text/markdown",
