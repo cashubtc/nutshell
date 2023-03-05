67ba9ba09732413e1697ec026d9832477125653c
callebtc <93376500+callebtc@users.noreply.github.com>
2023-03-05 11:09:01 +0100

    make format

--- cashu/mint/startup.py
+++ cashu/mint/startup.py
@@ -7,7 +7,7 @@ from loguru import logger
 
 from cashu.core.db import Database
 from cashu.core.migrations import migrate_databases
-from cashu.core.settings import CASHU_DIR, LIGHTNING, MINT_PRIVATE_KEY, MINT_DATABASE
+from cashu.core.settings import CASHU_DIR, LIGHTNING, MINT_DATABASE, MINT_PRIVATE_KEY
 from cashu.lightning.lnbits import LNbitsWallet  # type: ignore
 from cashu.mint import migrations
 from cashu.mint.ledger import Ledger
