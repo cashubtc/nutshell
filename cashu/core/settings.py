c2cff688b2c1910f08f4261de1de01d50ef34821
callebtc <93376500+callebtc@users.noreply.github.com>
2023-03-15 16:26:45 +0100

    remove old settings

--- cashu/core/settings.py
+++ cashu/core/settings.py
@@ -7,68 +7,68 @@ from environs import Env  # type: ignore
 
 env = Env()
 
-# env file: default to current dir, else home dir
-ENV_FILE = os.path.join(os.getcwd(), ".env")
-if not os.path.isfile(ENV_FILE):
-    ENV_FILE = os.path.join(str(Path.home()), ".cashu", ".env")
-if os.path.isfile(ENV_FILE):
-    env.read_env(ENV_FILE)
-else:
-    ENV_FILE = ""
-    env.read_env(recurse=False)
+# # env file: default to current dir, else home dir
+# ENV_FILE = os.path.join(os.getcwd(), ".env")
+# if not os.path.isfile(ENV_FILE):
+#     ENV_FILE = os.path.join(str(Path.home()), ".cashu", ".env")
+# if os.path.isfile(ENV_FILE):
+#     env.read_env(ENV_FILE)
+# else:
+#     ENV_FILE = ""
+#     env.read_env(recurse=False)
 
-DEBUG = env.bool("DEBUG", default=False)
-if not DEBUG:
-    sys.tracebacklimit = 0
+# DEBUG = env.bool("DEBUG", default=False)
+# if not DEBUG:
+#     sys.tracebacklimit = 0
 
-CASHU_DIR = env.str("CASHU_DIR", default=os.path.join(str(Path.home()), ".cashu"))
-CASHU_DIR = CASHU_DIR.replace("~", str(Path.home()))
-assert len(CASHU_DIR), "CASHU_DIR not defined"
+# CASHU_DIR = env.str("CASHU_DIR", default=os.path.join(str(Path.home()), ".cashu"))
+# CASHU_DIR = CASHU_DIR.replace("~", str(Path.home()))
+# assert len(CASHU_DIR), "CASHU_DIR not defined"
 
-TOR = env.bool("TOR", default=True)
+# TOR = env.bool("TOR", default=True)
 
-SOCKS_HOST = env.str("SOCKS_HOST", default=None)
-SOCKS_PORT = env.int("SOCKS_PORT", default=9050)
+# SOCKS_HOST = env.str("SOCKS_HOST", default=None)
+# SOCKS_PORT = env.int("SOCKS_PORT", default=9050)
 
-LIGHTNING = env.bool("LIGHTNING", default=True)
-LIGHTNING_FEE_PERCENT = env.float("LIGHTNING_FEE_PERCENT", default=1.0)
-assert LIGHTNING_FEE_PERCENT >= 0, "LIGHTNING_FEE_PERCENT must be at least 0"
-LIGHTNING_RESERVE_FEE_MIN = env.float("LIGHTNING_RESERVE_FEE_MIN", default=2000)
+# LIGHTNING = env.bool("LIGHTNING", default=True)
+# LIGHTNING_FEE_PERCENT = env.float("LIGHTNING_FEE_PERCENT", default=1.0)
+# assert LIGHTNING_FEE_PERCENT >= 0, "LIGHTNING_FEE_PERCENT must be at least 0"
+# LIGHTNING_RESERVE_FEE_MIN = env.float("LIGHTNING_RESERVE_FEE_MIN", default=2000)
 
-MINT_PRIVATE_KEY = env.str("MINT_PRIVATE_KEY", default=None)
+# MINT_PRIVATE_KEY = env.str("MINT_PRIVATE_KEY", default=None)
 
-MINT_SERVER_HOST = env.str("MINT_SERVER_HOST", default="127.0.0.1")
-MINT_SERVER_PORT = env.int("MINT_SERVER_PORT", default=3338)
+# MINT_SERVER_HOST = env.str("MINT_SERVER_HOST", default="127.0.0.1")
+# MINT_SERVER_PORT = env.int("MINT_SERVER_PORT", default=3338)
 
-MINT_URL = env.str("MINT_URL", default=None)
-MINT_HOST = env.str("MINT_HOST", default="8333.space")
-MINT_PORT = env.int("MINT_PORT", default=3338)
+# MINT_URL = env.str("MINT_URL", default=None)
+# MINT_HOST = env.str("MINT_HOST", default="8333.space")
+# MINT_PORT = env.int("MINT_PORT", default=3338)
 
-MINT_LIGHTNING_BACKEND = env.str("MINT_LIGHTNING_BACKEND", default="FakeWallet")
-MINT_DATABASE = env.str("MINT_DATABASE", default="data/mint")
+# MINT_LIGHTNING_BACKEND = env.str("MINT_LIGHTNING_BACKEND", default="FakeWallet")
+# MINT_DATABASE = env.str("MINT_DATABASE", default="data/mint")
 
-if not MINT_URL:
-    if MINT_HOST in ["localhost", "127.0.0.1"]:
-        MINT_URL = f"http://{MINT_HOST}:{MINT_PORT}"
-    else:
-        MINT_URL = f"https://{MINT_HOST}:{MINT_PORT}"
+# if not MINT_URL:
+#     if MINT_HOST in ["localhost", "127.0.0.1"]:
+#         MINT_URL = f"http://{MINT_HOST}:{MINT_PORT}"
+#     else:
+#         MINT_URL = f"https://{MINT_HOST}:{MINT_PORT}"
 
-LNBITS_ENDPOINT = env.str("LNBITS_ENDPOINT", default=None)
-LNBITS_KEY = env.str("LNBITS_KEY", default=None)
+# LNBITS_ENDPOINT = env.str("LNBITS_ENDPOINT", default=None)
+# LNBITS_KEY = env.str("LNBITS_KEY", default=None)
 
-NOSTR_PRIVATE_KEY = env.str("NOSTR_PRIVATE_KEY", default=None)
-NOSTR_RELAYS = env.list(
-    "NOSTR_RELAYS",
-    default=[
-        "wss://nostr-pub.wellorder.net",
-        "wss://relay.damus.io",
-        "wss://nostr.zebedee.cloud",
-        "wss://relay.snort.social",
-        "wss://nostr.fmt.wiz.biz",
-    ],
-)
+# NOSTR_PRIVATE_KEY = env.str("NOSTR_PRIVATE_KEY", default=None)
+# NOSTR_RELAYS = env.list(
+#     "NOSTR_RELAYS",
+#     default=[
+#         "wss://nostr-pub.wellorder.net",
+#         "wss://relay.damus.io",
+#         "wss://nostr.zebedee.cloud",
+#         "wss://relay.snort.social",
+#         "wss://nostr.fmt.wiz.biz",
+#     ],
+# )
 
-MAX_ORDER = 64
+# MAX_ORDER = 64
 VERSION = "0.9.4"
 
 
@@ -115,7 +115,7 @@ class MintSettings(CashuSettings):
     mint_private_key: str = Field(default=None)
     mint_listen_host: str = Field(default="127.0.0.1")
     mint_listen_port: int = Field(default=3338)
-    mint_lightning_backend: str = Field(default="FakeWallet")
+    mint_lightning_backend: str = Field(default="LNbitsWallet")
     mint_database: str = Field(default="data/mint")
 
     mint_lnbits_endpoint: str = Field(default=None)
@@ -166,11 +166,11 @@ def startup_settings_tasks():
 
     # set mint_url if only mint_host is set
     if not settings.mint_url:
-        if settings.mint_host in ["localhost", "127.0.0.1"]:
+        if settings.mint_host in ["localhost", "127.0.0.1"] and settings.mint_port:
             # localhost without https
-            settings.mint_url = f"http://{MINT_HOST}:{MINT_PORT}"
+            settings.mint_url = f"http://{settings.mint_host}:{settings.mint_port}"
         else:
-            settings.mint_url = f"https://{MINT_HOST}:{MINT_PORT}"
+            settings.mint_url = f"https://{settings.mint_host}:{settings.mint_port}"
 
 
 startup_settings_tasks()
