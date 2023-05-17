5d2491a4b5b72eb3c8d49afcc6d35c4bc8f1b98c
AngusP <angus@toaster.cc>
2023-05-18 00:26:46 +0100

    LNBits integration: use aiohttp instead of requests

--- cashu/lightning/lnbits.py
+++ cashu/lightning/lnbits.py
@@ -1,7 +1,7 @@
 # type: ignore
 from typing import Dict, Optional
 
-import requests
+import aiohttp
 
 from ..core.settings import settings
 from .base import (
@@ -21,14 +21,17 @@ class LNbitsWallet(Wallet):
 
         key = settings.mint_lnbits_key
         self.key = {"X-Api-Key": key}
-        self.s = requests.Session()
-        self.s.auth = ("user", "pass")
+        if settings.debug:
+            connector = aiohttp.TCPConnector(ssl=False)
+        else:
+            connector = None
+        self.s = aiohttp.ClientSession(connector=connector)
+        # self.s.auth = ("user", "pass")
         self.s.headers.update({"X-Api-Key": key})
-        self.s.verify = not settings.debug
 
     async def status(self) -> StatusResponse:
         try:
-            r = self.s.get(url=f"{self.endpoint}/api/v1/wallet", timeout=15)
+            r = await self.s.get(url=f"{self.endpoint}/api/v1/wallet", timeout=15)
             r.raise_for_status()
         except Exception as exc:
             return StatusResponse(
@@ -36,10 +39,10 @@ class LNbitsWallet(Wallet):
             )
 
         try:
-            data = r.json()
+            data = await r.json()
         except:
             return StatusResponse(
-                f"Failed to connect to {self.endpoint}, got: '{r.text[:200]}...'", 0
+                f"Failed to read from {self.endpoint}, got non-JSON or empty reponse", 0
             )
         if "detail" in data:
             return StatusResponse(f"LNbits error: {data['detail']}", 0)
@@ -60,10 +63,15 @@ class LNbitsWallet(Wallet):
 
         data["memo"] = memo or ""
         try:
-            r = self.s.post(url=f"{self.endpoint}/api/v1/payments", json=data)
+            r = await self.s.post(url=f"{self.endpoint}/api/v1/payments", json=data)
+        except:
+            return InvoiceResponse(False, None, None, "Request error.")
+        try:
             r.raise_for_status()
         except:
-            return InvoiceResponse(False, None, None, r.json()["detail"])
+            data = await r.json()
+            return InvoiceResponse(False, None, None, data["detail"])
+
         ok, checking_id, payment_request, error_message = (
             True,
             None,
@@ -71,26 +79,32 @@ class LNbitsWallet(Wallet):
             None,
         )
 
-        data = r.json()
+        data = await r.json()
         checking_id, payment_request = data["checking_id"], data["payment_request"]
 
         return InvoiceResponse(ok, checking_id, payment_request, error_message)
 
     async def pay_invoice(self, bolt11: str, fee_limit_msat: int) -> PaymentResponse:
         try:
-            r = self.s.post(
+            r = await self.s.post(
                 url=f"{self.endpoint}/api/v1/payments",
                 json={"out": True, "bolt11": bolt11},
                 timeout=None,
             )
+        except:
+            return PaymentResponse(None, None, None, None, "Request error.")
+        try:
             r.raise_for_status()
         except:
-            error_message = r.json()["detail"]
+            error_message = (await r.json())["detail"]
             return PaymentResponse(None, None, None, None, error_message)
-        if r.status_code > 299:
+        if r.status > 299:
             return PaymentResponse(None, None, None, None, f"HTTP status: {r.reason}")
-        if "detail" in r.json():
-            return PaymentResponse(None, None, None, None, r.json()["detail"])
+        data = await r_json()
+
+        if "detail" in data:
+            return PaymentResponse(None, None, None, None, data["detail"])
+
         ok, checking_id, fee_msat, preimage, error_message = (
             True,
             None,
@@ -99,7 +113,6 @@ class LNbitsWallet(Wallet):
             None,
         )
 
-        data = r.json()
         checking_id = data["payment_hash"]
 
         # we do this to get the fee and preimage
@@ -109,26 +122,29 @@ class LNbitsWallet(Wallet):
 
     async def get_invoice_status(self, checking_id: str) -> PaymentStatus:
         try:
-            r = self.s.get(
+            r = await self.s.get(
                 url=f"{self.endpoint}/api/v1/payments/{checking_id}",
                 headers=self.key,
             )
             r.raise_for_status()
         except:
             return PaymentStatus(None)
-        if r.json().get("detail"):
+
+        data = await r.json()
+        if data.get("detail"):
             return PaymentStatus(None)
-        return PaymentStatus(r.json()["paid"])
+        return PaymentStatus(data["paid"])
 
     async def get_payment_status(self, checking_id: str) -> PaymentStatus:
         try:
-            r = self.s.get(
+            r = await self.s.get(
                 url=f"{self.endpoint}/api/v1/payments/{checking_id}", headers=self.key
             )
             r.raise_for_status()
         except:
             return PaymentStatus(None)
-        data = r.json()
+
+        data = await r.json()
         if "paid" not in data and "details" not in data:
             return PaymentStatus(None)
 
