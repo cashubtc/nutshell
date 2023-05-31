bafb6d056fb590ca56813c3477d3384b006aa8fc
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2023-06-01 00:09:46 +0200

    simplify `amount_split` by iterating over amount bits
    Small optimization: rather than involving a string data type here (which
    needs to also be reversed and sliced), we can just directly operate on
    the amount integer variable by iterating over all the bits and add 2^i
    to the return list if bit i is set.
    
    Also, add type hints to the parameter / return value and assert that
    the passed value must not be negative.

--- cashu/core/split.py
+++ cashu/core/split.py
@@ -1,8 +1,13 @@
-def amount_split(amount):
+from typing import List
+
+
+def amount_split(amount: int) -> List[int]:
     """Given an amount returns a list of amounts returned e.g. 13 is [1, 4, 8]."""
-    bits_amt = bin(amount)[::-1][:-2]
+    if amount < 0:
+        raise Exception("can't split negative amount")
+    assert amount >= 0
     rv = []
-    for (pos, bit) in enumerate(bits_amt):
-        if bit == "1":
-            rv.append(2**pos)
+    for i in range(amount.bit_length()):
+        if amount & (1 << i):  # if bit i is set, add 2**i to list
+            rv.append(1 << i)
     return rv
