a407d169bd730f6ab6566c06d765d49b1e65a4c1
sihamon <sihamon@proton.me>
2023-06-06 20:01:10 +0200

    Sort imports correctly

--- cashu/core/base.py
+++ cashu/core/base.py
@@ -1,7 +1,7 @@
 import base64
 import json
-from typing import Any, Dict, List, Optional, Union
 from sqlite3 import Row
+from typing import Any, Dict, List, Optional, Union
 
 from loguru import logger
 from pydantic import BaseModel
