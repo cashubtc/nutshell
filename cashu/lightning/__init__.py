a5f7270736857eb3f1e3bb6ec0bedf1f68e000d2
dni ⚡ <office@dnilabs.com>
2023-06-29 15:21:11 +0200

    E265, E261

--- cashu/lightning/__init__.py
+++ cashu/lightning/__init__.py
@@ -1,3 +1,3 @@
-#type: ignore
-from .fake import FakeWallet # noqa: F401
-from .lnbits import LNbitsWallet # noqa: F401
+# type: ignore
+from .fake import FakeWallet  # noqa: F401
+from .lnbits import LNbitsWallet  # noqa: F401
