5e274389e1ba02c9c8491cc25fb0ed3f38b4074a
dni ⚡ <office@dnilabs.com>
2023-08-06 14:17:16 +0200

    async test werent marked async

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -313,6 +313,7 @@ async def test_p2sh_receive_with_wrong_wallet(wallet1: Wallet, wallet2: Wallet):
     await assert_err(wallet2.redeem(send_proofs), "lock not found.")  # wrong receiver
 
 
+@pytest.mark.asyncio
 async def test_token_state(wallet1: Wallet):
     await wallet1.mint(64)
     assert wallet1.balance == 64
@@ -321,6 +322,7 @@ async def test_token_state(wallet1: Wallet):
     assert resp.dict()["pending"]
 
 
+@pytest.mark.asyncio
 async def test_bump_secret_derivation(wallet3: Wallet):
     await wallet3._init_private_key(
         "half depart obvious quality work element tank gorilla view sugar picture humble"
