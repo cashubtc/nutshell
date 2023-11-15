2b1518da661784415522d38f1abfa93868a7d6b7
callebtc <93376500+callebtc@users.noreply.github.com>
2023-11-14 21:22:39 -0300

    check if wallet is set

--- cashu/lightning/__init__.py
+++ cashu/lightning/__init__.py
@@ -1,4 +1,8 @@
 # type: ignore
+from ..core.settings import settings
 from .fake import FakeWallet  # noqa: F401
 from .lnbits import LNbitsWallet  # noqa: F401
 from .lndrest import LndRestWallet  # noqa: F401
+
+if settings.mint_lightning_backend is None:
+    raise Exception("MINT_LIGHTNING_BACKEND not configured")
