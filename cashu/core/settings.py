464c0e0ea4bb31ecb045dd5c38a4bd6e33e917c8
callebtc <93376500+callebtc@users.noreply.github.com>
2024-02-11 20:23:43 +0100

    DB backups before migrations (#420)
    * make backups before migrations
    
    * database tests
    
    * postgres db backup with location string
    
    * ignore version in pg_dump and throw warning
    
    * install latest pg_dump
    
    * install latest
    
    * pg update?
    
    * remove test from github
    
    * skip for postgres on github actions

--- cashu/core/settings.py
+++ cashu/core/settings.py
@@ -45,6 +45,7 @@ class EnvSettings(CashuSettings):
     cashu_dir: str = Field(default=os.path.join(str(Path.home()), ".cashu"))
     debug_profiling: bool = Field(default=False)
     debug_mint_only_deprecated: bool = Field(default=False)
+    db_backup_path: str = Field(default=False)
 
 
 class MintSettings(CashuSettings):
