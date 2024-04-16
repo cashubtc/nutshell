eabc5421f9770ca70fd24abd28ced745186d77d7
callebtc <93376500+callebtc@users.noreply.github.com>
2024-04-16 17:37:41 +0200

    default setting for mint_derivation_path_list

--- cashu/core/settings.py
+++ cashu/core/settings.py
@@ -52,7 +52,7 @@ class MintSettings(CashuSettings):
     mint_private_key: str = Field(default=None)
     mint_seed_decryption_key: Optional[str] = Field(default=None)
     mint_derivation_path: str = Field(default="m/0'/0'/0'")
-    mint_derivation_path_list: List[str] = Field(default=[""])
+    mint_derivation_path_list: List[str] = Field(default=[])
     mint_listen_host: str = Field(default="127.0.0.1")
     mint_listen_port: int = Field(default=3338)
 
