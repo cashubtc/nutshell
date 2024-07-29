dee664bd6299d743bcb4f2e575466f8af51c9cd8
lollerfirst <43107113+lollerfirst@users.noreply.github.com>
2024-07-29 16:49:41 +0200

    Update cashu/core/settings.py
    Co-authored-by: callebtc <93376500+callebtc@users.noreply.github.com>

--- cashu/core/settings.py
+++ cashu/core/settings.py
@@ -205,7 +205,6 @@ class LndRPCFundingSource(MintSettings):
     mint_lnd_rpc_endpoint: Optional[str] = Field(default=None)
     mint_lnd_rpc_cert: Optional[str] = Field(default=None)
     mint_lnd_rpc_macaroon: Optional[str] = Field(default=None)
-    mint_lnd_enable_mpp: bool = Field(default=False)
 
 class CLNRestFundingSource(MintSettings):
     mint_clnrest_url: Optional[str] = Field(default=None)
