a8fcec5c8e8dfe12734379cdcdf27075bf767a69
Pavol Rusnak <pavol@rusnak.io>
2024-09-02 17:29:58 +0200

    implement nut-06 time

--- cashu/wallet/mint_info.py
+++ cashu/wallet/mint_info.py
@@ -15,6 +15,7 @@ class MintInfo(BaseModel):
     description_long: Optional[str]
     contact: Optional[List[MintInfoContact]]
     motd: Optional[str]
+    time: Optional[int]
     nuts: Optional[Dict[int, Any]]
 
     def __str__(self):
