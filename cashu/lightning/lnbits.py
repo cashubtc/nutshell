f8f061f810170e98bd757c14053b3707db40fc6d
callebtc <93376500+callebtc@users.noreply.github.com>
2024-10-02 15:13:17 +0200

    Fix: Unset pending melt quote by quote id (#629)
    * unser melt quote based on quote id
    
    * fixes for lnbits

--- cashu/lightning/lnbits.py
+++ cashu/lightning/lnbits.py
@@ -113,21 +113,21 @@ class LNbitsWallet(LightningBackend):
             )
             r.raise_for_status()
         except Exception:
+            error_message = r.json().get("detail") or r.reason_phrase
             return PaymentResponse(
-                result=PaymentResult.FAILED, error_message=r.json()["detail"]
+                result=PaymentResult.FAILED, error_message=error_message
             )
-        if r.status_code > 299:
-            return PaymentResponse(
-                result=PaymentResult.FAILED,
-                error_message=(f"HTTP status: {r.reason_phrase}",),
-            )
-        if "detail" in r.json():
+        if r.json().get("detail"):
             return PaymentResponse(
                 result=PaymentResult.FAILED, error_message=(r.json()["detail"],)
             )
 
         data: dict = r.json()
-        checking_id = data["payment_hash"]
+        checking_id = data.get("payment_hash")
+        if not checking_id:
+            return PaymentResponse(
+                result=PaymentResult.UNKNOWN, error_message="No payment_hash received"
+            )
 
         # we do this to get the fee and preimage
         payment: PaymentStatus = await self.get_payment_status(checking_id)
