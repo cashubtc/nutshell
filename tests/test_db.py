35692bec277ee5157cb2fbcb74544e3cde87b25c
callebtc <93376500+callebtc@users.noreply.github.com>
2024-11-01 14:55:06 +0100

    trigger

--- tests/test_db.py
+++ tests/test_db.py
@@ -147,7 +147,7 @@ async def test_db_get_connection_locked(wallet: Wallet, ledger: Ledger):
 @pytest.mark.asyncio
 @pytest.mark.skipif(
     not settings.mint_database.startswith("postgres"),
-    reason="SQLite does not support row locking",
+    reason="SQLite does not support row locking ",
 )
 async def test_db_get_connection_lock_row(wallet: Wallet, ledger: Ledger):
     if ledger.db.type == db.SQLITE:
