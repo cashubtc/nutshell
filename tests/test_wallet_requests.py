a586a2d9ae8edaaaff9e1fe1db5d63f44ccbf07a
callebtc <93376500+callebtc@users.noreply.github.com>
2024-11-05 14:49:42 +0100

    clean up test

--- tests/test_wallet_requests.py
+++ tests/test_wallet_requests.py
@@ -1,61 +1,16 @@
 import json
-from typing import List, Union
 
 import pytest
 import pytest_asyncio
 import respx
 from httpx import Request, Response
 
-from cashu.core.base import BlindedSignature, Proof
+from cashu.core.base import BlindedSignature
 from cashu.core.crypto.b_dhke import hash_to_curve
-from cashu.core.errors import CashuError
 from cashu.wallet.wallet import Wallet
 from cashu.wallet.wallet import Wallet as Wallet1
-from cashu.wallet.wallet import Wallet as Wallet2
 from tests.conftest import SERVER_ENDPOINT
 from tests.helpers import pay_if_regtest
-
-
-async def assert_err(f, msg: Union[str, CashuError]):
-    """Compute f() and expect an error message 'msg'."""
-    try:
-        await f
-    except Exception as exc:
-        error_message: str = str(exc.args[0])
-        if isinstance(msg, CashuError):
-            if msg.detail not in error_message:
-                raise Exception(
-                    f"CashuError. Expected error: {msg.detail}, got: {error_message}"
-                )
-            return
-        if msg not in error_message:
-            raise Exception(f"Expected error: {msg}, got: {error_message}")
-        return
-    raise Exception(f"Expected error: {msg}, got no error")
-
-
-async def assert_err_multiple(f, msgs: List[str]):
-    """Compute f() and expect an error message 'msg'."""
-    try:
-        await f
-    except Exception as exc:
-        for msg in msgs:
-            if msg in str(exc.args[0]):
-                return
-        raise Exception(f"Expected error: {msgs}, got: {exc.args[0]}")
-    raise Exception(f"Expected error: {msgs}, got no error")
-
-
-def assert_amt(proofs: List[Proof], expected: int):
-    """Assert amounts the proofs contain."""
-    assert sum([p.amount for p in proofs]) == expected
-
-
-async def reset_wallet_db(wallet: Wallet):
-    await wallet.db.execute("DELETE FROM proofs")
-    await wallet.db.execute("DELETE FROM proofs_used")
-    await wallet.db.execute("DELETE FROM keysets")
-    await wallet.load_mint()
 
 
 @pytest_asyncio.fixture(scope="function")
@@ -67,17 +22,6 @@ async def wallet1(mint):
     )
     await wallet1.load_mint()
     yield wallet1
-
-
-@pytest_asyncio.fixture(scope="function")
-async def wallet2():
-    wallet2 = await Wallet2.with_db(
-        url=SERVER_ENDPOINT,
-        db="test_data/wallet2",
-        name="wallet2",
-    )
-    await wallet2.load_mint()
-    yield wallet2
 
 
 @pytest.mark.asyncio
