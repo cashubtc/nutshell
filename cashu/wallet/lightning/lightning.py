3c6b125862908ce628edec6854b275cdb568b719
lollerfirst <lollerfirst@gmail.com>
2024-11-20 17:50:15 +0100

    fix `get_invoice_status`

--- cashu/wallet/lightning/lightning.py
+++ cashu/wallet/lightning/lightning.py
@@ -108,7 +108,7 @@ class LightningWallet(Wallet):
             return PaymentStatus(result=PaymentResult.SETTLED)
         try:
             # to check the invoice state, we try minting tokens
-            await self.mint(mint_quote.amount, quote_id=mint_quote.quote)
+            await self.mint(mint_quote.amount, quote_id=mint_quote.quote, quote_privkey=mint_quote.privkey)
             return PaymentStatus(result=PaymentResult.SETTLED)
         except Exception as e:
             print(e)
