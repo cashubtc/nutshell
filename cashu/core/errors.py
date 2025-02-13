83362538eb4bcc573e6aeff27cee0a3d198eef52
lollerfirst <lollerfirst@gmail.com>
2025-02-13 14:52:56 +0100

    add new error codes

--- cashu/core/errors.py
+++ cashu/core/errors.py
@@ -86,6 +86,34 @@ class TransactionAmountExceedsLimitError(TransactionError):
     def __init__(self, detail):
         super().__init__(detail, code=self.code)
 
+class TransactionDuplicateInputsError(TransactionError):
+    detail = "Duplicate inputs provided"
+    code = 11007
+
+    def __init__(self, detail: Optional[str] = None):
+        super().__init__(detail, code=self.code)
+
+class TransactionDuplicateOutputsError(TransactionError):
+    detail = "Duplicate outputs provided"
+    code = 11008
+
+    def __init__(self, detail: Optional[str] = None):
+        super().__init__(detail, code=self.code)
+
+class TransactionMultipleUnitsError(TransactionError):
+    detail = "Inputs/Outputs of multiple units"
+    code = 11009
+
+    def __init__(self, detail: Optional[str] = None):
+        super().__init__(detail, code=self.code)
+
+class TransactionUnitMismatchError(TransactionError):
+    detail = "Inputs and outputs not of same unit"
+    code = 11010
+
+    def __init__(self, detail: Optional[str] = None):
+        super().__init__(detail, code=self.code)
+
 
 class KeysetError(CashuError):
     detail = "keyset error"
