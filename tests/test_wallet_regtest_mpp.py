9fe5d23c4558d27dbb836814207f4a13f878c350
lollerfirst <lollerfirst@gmail.com>
2025-02-14 13:31:54 +0100

    fix kw arg in regtest test

--- tests/test_wallet_regtest_mpp.py
+++ tests/test_wallet_regtest_mpp.py
@@ -52,7 +52,7 @@ async def test_regtest_pay_mpp(wallet: Wallet, ledger: Ledger):
 
     async def _mint_pay_mpp(invoice: str, amount: int, proofs: List[Proof]):
         # wallet pays 32 sat of the invoice
-        quote = await wallet.melt_quote(invoice, amount=amount*1000)
+        quote = await wallet.melt_quote(invoice, amount_msat=amount*1000)
         assert quote.amount == amount
         await wallet.melt(
             proofs,
@@ -118,7 +118,7 @@ async def test_regtest_pay_mpp_incomplete_payment(wallet: Wallet, ledger: Ledger
     async def pay_mpp(amount: int, proofs: List[Proof], delay: float = 0.0):
         await asyncio.sleep(delay)
         # wallet pays 32 sat of the invoice
-        quote = await wallet.melt_quote(invoice_payment_request, amount=amount*1000)
+        quote = await wallet.melt_quote(invoice_payment_request, amount_msat=amount*1000)
         assert quote.amount == amount
         await wallet.melt(
             proofs,
