5bb22cf2649140ade6d0b846ebf92b94499ba503
lollerfirst <lollerfirst@gmail.com>
2025-02-25 00:57:21 +0100

    clnrest

--- cashu/lightning/clnrest.py
+++ cashu/lightning/clnrest.py
@@ -329,16 +329,25 @@ class CLNRestWallet(LightningBackend):
     async def get_payment_quote(
         self, melt_quote: PostMeltQuoteRequest
     ) -> PaymentQuoteResponse:
+
         invoice_obj = decode(melt_quote.request)
-        assert invoice_obj.amount_msat, "invoice has no amount."
-        assert invoice_obj.amount_msat > 0, "invoice has 0 amount."
-        amount_msat = invoice_obj.amount_msat
+
+        # Detect and handle amountless request
+        amount_msat = 0
+        if melt_quote.is_amountless:
+            amount_msat = melt_quote.options.amountless.amount_msat
+        elif invoice_obj.amount_msat:
+            amount_msat = invoice_obj.amount_msat
+        else:
+            raise TransactionError("request has no amount and is not specified as amountless")
+
         if melt_quote.is_mpp:
             amount_msat = (
                 Amount(Unit[melt_quote.unit], melt_quote.mpp_amount)
                 .to(Unit.msat)
                 .amount
             )
+
         fees_msat = fee_reserve(amount_msat)
         fees = Amount(unit=Unit.msat, amount=fees_msat)
         amount = Amount(unit=Unit.msat, amount=amount_msat)
