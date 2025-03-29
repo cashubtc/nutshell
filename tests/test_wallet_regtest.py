9c9a56aae4a63720318ac8aa9229ec9ab08f7180
callebtc <93376500+callebtc@users.noreply.github.com>
2025-03-29 22:17:28 +0100

    test set proofs unreserved

--- tests/test_wallet_regtest.py
+++ tests/test_wallet_regtest.py
@@ -5,6 +5,7 @@ import pytest
 import pytest_asyncio
 
 from cashu.mint.ledger import Ledger
+from cashu.wallet.crud import get_proofs
 from cashu.wallet.wallet import Wallet
 from tests.conftest import SERVER_ENDPOINT
 from tests.helpers import (
@@ -104,3 +105,60 @@ async def test_regtest_failed_quote(wallet: Wallet, ledger: Ledger):
 
     states = await wallet.check_proof_state(send_proofs)
     assert all([s.unspent for s in states.states])
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(is_fake, reason="only regtest")
+async def test_regtest_melt_fail_restore_pending_batch_check(
+    wallet: Wallet, ledger: Ledger
+):
+    # fill wallet
+    mint_quote = await wallet.request_mint(64)
+    await pay_if_regtest(mint_quote.request)
+    await wallet.mint(64, quote_id=mint_quote.quote)
+    assert wallet.balance == 64
+
+    # create hodl invoice
+    preimage, invoice_dict = get_hold_invoice(16)
+    invoice_payment_request = str(invoice_dict["payment_request"])
+    invoice_obj = bolt11.decode(invoice_payment_request)
+    preimage_hash = invoice_obj.payment_hash
+
+    # wallet pays the invoice
+    quote = await wallet.melt_quote(invoice_payment_request)
+    total_amount = quote.amount + quote.fee_reserve
+    _, send_proofs = await wallet.swap_to_send(
+        wallet.proofs, total_amount, set_reserved=True
+    )
+
+    # verify that the proofs are reserved
+    proofs_db = await get_proofs(db=wallet.db, melt_id=quote.quote)
+    assert all([p.reserved for p in proofs_db])
+
+    asyncio.create_task(
+        wallet.melt(
+            proofs=send_proofs,
+            invoice=invoice_payment_request,
+            fee_reserve_sat=quote.fee_reserve,
+            quote_id=quote.quote,
+        )
+    )
+    await asyncio.sleep(SLEEP_TIME)
+
+    states = await wallet.check_proof_state(send_proofs)
+    assert all([s.pending for s in states.states])
+
+    cancel_invoice(preimage_hash=preimage_hash)
+
+    await asyncio.sleep(SLEEP_TIME)
+
+    # test get_spent_proofs_check_states_batched: verify that no proofs are spent
+    spent_proofs = await wallet.get_spent_proofs_check_states_batched(send_proofs)
+    assert len(spent_proofs) == 0
+
+    # test get_melt_quote: verify that the proofs are not reserved anymore
+    quote_later = await wallet.get_melt_quote(quote.quote)
+    assert quote_later
+    assert quote_later.paid is False
+    proofs_db_later = await get_proofs(db=wallet.db, melt_id=quote.quote)
+    assert all([p.reserved is False for p in proofs_db_later])
