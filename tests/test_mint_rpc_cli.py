23ab50da5b4bea116ebbb1ca6951504787dbfc22
lollerfirst <lollerfirst@gmail.com>
2025-04-23 09:27:14 +0200

    format checks

--- tests/test_mint_rpc_cli.py
+++ tests/test_mint_rpc_cli.py
@@ -1,14 +1,10 @@
-import asyncio
-from typing import Tuple
 
-import bolt11
 import pytest
 from click.testing import CliRunner
 
 from cashu.core.settings import settings
 from cashu.mint.management_rpc.cli.cli import cli
-from cashu.wallet.wallet import Wallet
-from tests.helpers import is_deprecated_api_only, is_fake, is_regtest, pay_if_regtest
+
 
 @pytest.fixture(autouse=True, scope="session")
 def cli_prefix():
