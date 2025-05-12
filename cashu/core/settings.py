f82bf2597da4861a9011da0e96a9777e35d2359f
lollerfirst <lollerfirst@gmail.com>
2025-05-12 11:01:18 +0200

    remove duplicated settings

--- cashu/core/settings.py
+++ cashu/core/settings.py
@@ -175,21 +175,6 @@ class MintLimits(MintSettings):
         title="Maximum mint balance (EUR)",
         description="Maximum mint balance in Euros.",
     )
-
-    # DEPRECATED v0.16.5
-    mint_max_peg_in: int = Field(
-        default=None,
-        gt=0,
-        title="Maximum peg-in",
-        description="Maximum amount for a mint operation.",
-    )
-    # DEPRECATED v0.16.5
-    mint_max_peg_out: int = Field(
-        default=None,
-        gt=0,
-        title="Maximum peg-out",
-        description="Maximum amount for a melt operation.",
-    )
     # Unit-specific maximum mint settings
     mint_max_sat_mint: int = Field(
         default=None,
