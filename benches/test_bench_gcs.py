1e6efd06e40b8c4eff73a86d79f536108be9a518
lollerfirst <lollerfirst@gmail.com>
2025-05-17 14:41:40 +0200

    feat: add sorting benchmarks for GCSFilter using hash_to_range

--- benches/test_bench_gcs.py
+++ benches/test_bench_gcs.py
@@ -1,5 +1,5 @@
 import os
-from cashu.core.gcs import GCSFilter
+from cashu.core.gcs import GCSFilter, hash_to_range
 
 num_items_arr = [100, 1000, 10000, 100000, 1000000]
 item_size = 33
@@ -23,3 +23,39 @@ def test_gcs_filter_creation_benchmark_100000(benchmark):
 def test_gcs_filter_creation_benchmark_1000000(benchmark):
     items = [os.urandom(item_size) for _ in range(num_items_arr[4])]
     benchmark(GCSFilter.create, items)
+
+def test_sort_benchmark_100(benchmark):
+    items = [os.urandom(item_size) for _ in range(num_items_arr[0])]
+    f = 784931 * len(items)
+    hashed_items = [hash_to_range(item, f) for item in items]
+
+    benchmark(sorted, hashed_items)
+
+def test_sort_benchmark_1000(benchmark):
+    items = [os.urandom(item_size) for _ in range(num_items_arr[1])]
+    f = 784931 * len(items)
+    hashed_items = [hash_to_range(item, f) for item in items]
+
+    benchmark(sorted, hashed_items)
+
+def test_sort_benchmark_10000(benchmark):
+    items = [os.urandom(item_size) for _ in range(num_items_arr[2])]
+    f = 784931 * len(items)
+    hashed_items = [hash_to_range(item, f) for item in items]
+
+    benchmark(sorted, hashed_items)
+
+def test_sort_benchmark_100000(benchmark):
+    items = [os.urandom(item_size) for _ in range(num_items_arr[3])]
+    f = 784931 * len(items)
+    hashed_items = [hash_to_range(item, f) for item in items]
+
+    benchmark(sorted, hashed_items)
+
+
+def test_sort_benchmark_1000000(benchmark):
+    items = [os.urandom(item_size) for _ in range(num_items_arr[4])]
+    f = 784931 * len(items)
+    hashed_items = [hash_to_range(item, f) for item in items]
+
+    benchmark(sorted, hashed_items)
