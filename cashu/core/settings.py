b880f87bd3a5be7c8acbc7aad1a991d95ac52d88
callebtc <93376500+callebtc@users.noreply.github.com>
2025-06-01 00:15:26 +0200

    adjust mint melt settings

--- cashu/core/settings.py
+++ cashu/core/settings.py
@@ -125,22 +125,45 @@ class MintLimits(MintSettings):
         description="Maximum length of REST API request arrays.",
     )
 
-    mint_peg_out_only: bool = Field(
+    mint_peg_out_only: bool = Field(  # deprecated for mint_bolt11_disable_mint
         default=False,
-        title="Peg-out only",
-        description="Mint allows no mint operations.",
+        title="Disable minting tokens with bolt11",
+        description="Mint allows no bolt11 minting operations.",
     )
-    mint_max_peg_in: int = Field(
+    mint_bolt11_disable_mint: bool = Field(
+        default=False,
+        title="Disable minting tokens with bolt11",
+        description="Mint allows no bolt11 minting operations.",
+    )
+    mint_bolt11_disable_melt: bool = Field(
+        default=False,
+        title="Disable melting tokens with bolt11",
+        description="Mint allows no bolt11 melting operations.",
+    )
+
+    mint_max_peg_in: int = Field(  # deprecated for mint_max_mint_bolt11_sat
         default=None,
         gt=0,
         title="Maximum peg-in",
         description="Maximum amount for a mint operation.",
     )
-    mint_max_peg_out: int = Field(
+    mint_max_peg_out: int = Field(  # deprecated for mint_max_melt_bolt11_sat
         default=None,
         gt=0,
         title="Maximum peg-out",
         description="Maximum amount for a melt operation.",
+    )
+    mint_max_mint_bolt11_sat: int = Field(
+        default=None,
+        gt=0,
+        title="Maximum mint amount for bolt11 in satoshis",
+        description="Maximum amount for a bolt11 mint operation in satoshis.",
+    )
+    mint_max_melt_bolt11_sat: int = Field(
+        default=None,
+        gt=0,
+        title="Maximum melt amount for bolt11 in satoshis",
+        description="Maximum amount for a bolt11 melt operation in satoshis.",
     )
     mint_max_balance: int = Field(
         default=None,
@@ -338,6 +361,16 @@ def startup_settings_tasks():
     # backwards compatibility: set mint_backend_bolt11_sat from mint_lightning_backend
     if settings.mint_lightning_backend:
         settings.mint_backend_bolt11_sat = settings.mint_lightning_backend
+
+    # backwards compatibility: mint_max_peg_in and mint_max_peg_out to mint_max_mint_bolt11_sat and mint_max_melt_bolt11_sat
+    if settings.mint_max_peg_in:
+        settings.mint_max_mint_bolt11_sat = settings.mint_max_peg_in
+    if settings.mint_max_peg_out:
+        settings.mint_max_melt_bolt11_sat = settings.mint_max_peg_out
+
+    # backwards compatibility: set mint_bolt11_disable_mint from mint_peg_out_only
+    if settings.mint_peg_out_only:
+        settings.mint_bolt11_disable_mint = True
 
 
 startup_settings_tasks()
