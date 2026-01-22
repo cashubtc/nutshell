fb9fa55dfaba5b9f2af76a05daa163b33bb8af4b
a1denvalu3 <cloudsupper@proton.me>
2026-01-22 11:01:20 +0100

    fix redis startup check

--- cashu/mint/cache.py
+++ cashu/mint/cache.py
@@ -20,7 +20,6 @@ class RedisCache:
             if settings.mint_redis_cache_url is None:
                 raise CashuError("Redis cache url not provided")
             self.redis = from_url(settings.mint_redis_cache_url)
-            asyncio.create_task(self.test_connection())
 
     async def test_connection(self):
         # PING
