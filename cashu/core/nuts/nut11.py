32803fc0284bcdce0438019739bb1a253a267a80
callebtc <93376500+callebtc@users.noreply.github.com>
2026-07-08 13:04:27 +0300

    fix: update sigall tests after main merge

--- cashu/core/nuts/nut11.py
+++ cashu/core/nuts/nut11.py
@@ -5,14 +5,13 @@ from ..base import BlindedMessage, Proof
 
 def sigall_message_to_sign(proofs: List[Proof], outputs: List[BlindedMessage]) -> str:
     """
-    Creates the message to sign for sigall spending conditions.
-    The message is a concatenation of all proof secrets and signatures + all output attributes (amount, id, B_).
+    Creates the message to sign for SIG_ALL spending conditions.
+
+    The message is the concatenation of each proof's secret and C fields,
+    followed by each output's amount and B_ fields.
     """
 
-    # Concatenate all proof secrets
     message = "".join([p.secret + p.C for p in proofs])
-
-    # Concatenate all output attributes
     message += "".join([str(o.amount) + o.B_ for o in outputs])
 
     return message
